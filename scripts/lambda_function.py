fe81dee69d8e301c841a1f5a39e7b1df28776e08
a1725473 <a1725473@student.adelaide.edu.au>
2024-04-18 16:57:10 +0930

    Update script to check if exists + git ignore

--- scripts/lambda_function.py
+++ scripts/lambda_function.py
@@ -9,13 +9,28 @@ tableName = 'website_resume'
 def lambda_handler(event, context):
     print('Event received...', event)
 
-    # Update the current view count
-    response = table.update_item(
-        Key={"id":"1"},
-        UpdateExpression="SET view_count = view_count + :val",
-        ExpressionAttributeValues={':val': 1},
-        ReturnValues="UPDATED_NEW"
+    # Check if item exists
+    response = table.get_item(
+        Key={
+            "id": "1",
+        }
     )
+
+    if response:
+        # Update the current view count
+        response = table.update_item(
+            Key={"id":"1"},
+            UpdateExpression="SET view_count = view_count + :val",
+            ExpressionAttributeValues={':val': 1},
+            ReturnValues="UPDATED_NEW"
+        )
+    else:
+        response = table.put_item(
+            Item = {
+                "id": "1",
+                "view_count": 1
+            }
+        )
     
     # Return the updated view count
     return {
