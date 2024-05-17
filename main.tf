7648925e9c3efbce0fd6f7998e4d23655106add1
Aaron Clark <aaron.clarky23@gmail.com>
2024-05-17 20:15:28 +0930

    Remove default init item

--- main.tf
+++ main.tf
@@ -19,14 +19,12 @@ terraform {
 }
 
 provider "aws" {
-  # profile = "default"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
   region  = "ap-southeast-2"
 }
 
 provider "aws" {
-  # profile = "default"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
   region  = "us-east-1"
