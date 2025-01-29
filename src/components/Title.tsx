0a3633bb7e548f71b502f39fed582795d1a66747
Aaron Clark <aaron@Aarons-Laptop.local>
2025-01-29 15:43:19 +1030

    Fix white/black css

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,7 @@ function Title() {
   return (
     <>
       {/* <p className="title-font">Aaron Clark</p> */}
-      <p className="text-xl">Aaron Clark</p>
+      <p className="title-font">Aaron Clark</p>
       <p className="text-base pb-3 md:pb-6">Developer</p>
       {/* <p className="subtext-font pb-3 md:pb-6">Developer</p> */}
     </>
