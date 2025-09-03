1a824b3e3fec3eb488fe8d7d4001fb0153e5fbb0
Aaron Clark <aaron@Aarons-Laptop.local>
2025-09-03 11:57:08 +0930

    Attempt to fix cert issue

--- cert.tf
+++ cert.tf
@@ -1,8 +1,13 @@
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.root_domain_name
   subject_alternative_names = [var.www_domain_name]
   validation_method = "DNS"
-  provider = aws.us
+  provider = aws.us-east-1
 
   lifecycle {
     create_before_destroy = true
