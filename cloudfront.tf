ff4d3d73c3b3fcd28c40909014a5eb80bce5ff84
Aaron Clark <aaron@Aarons-Laptop.local>
2025-09-03 12:12:13 +0930

    Reference certificate id var

--- cloudfront.tf
+++ cloudfront.tf
@@ -21,7 +21,7 @@ resource "aws_cloudfront_distribution" "distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.cert.arn
+    acm_certificate_arn = var.acm_certificate_arn
     ssl_support_method = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
